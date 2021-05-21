111a816de3ae04b6e1b8813438ee5b2bc02c756e
christinelant <christinelantigua@yahoo.com>
2021-05-21 01:31:32 -0400

    ddm file update

--- ddm_file.sql
+++ ddm_file.sql
@@ -30,3 +30,19 @@ UPDATE `payments` SET `paymentID`='[value-1]',`paidBy`='[value-2]',`amountPaid`=
 DELETE FROM `payments`
 
 --------------------------------------
+
+# employees
+--------------------------------------
+# shows all payments
+SELECT * FROM `payments`
+
+# inserts values
+INSERT INTO `payments`(`paymentID`, `paidBy`, `amountPaid`, `datePaid`, `paymentType`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]')
+
+# update values
+UPDATE `payments` SET `paymentID`='[value-1]',`paidBy`='[value-2]',`amountPaid`='[value-3]',`datePaid`='[value-4]',`paymentType`='[value-5]'
+
+# delete values
+DELETE FROM `payments`
+
+--------------------------------------
